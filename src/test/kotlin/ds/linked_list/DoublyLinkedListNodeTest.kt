e1cc7cd357bbda49876cb2ec0ec977260f1bdc31
Remon Shehata <RemonGerges94@gmail.com>
2022-02-02 00:11:08 +0200

    add head tests for DoublyLinkedList

--- src/test/kotlin/ds/linked_list/DoublyLinkedListNodeTest.kt
+++ src/test/kotlin/ds/linked_list/DoublyLinkedListNodeTest.kt
@@ -3,16 +3,62 @@ package ds.linked_list
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class DoublyLinkedListNodeTest{
+internal class DoublyLinkedListNodeTest {
 
     //region head and count tests
     @Test
-    fun `given empty list, the head is null, and count is 0`() {
-        val emptySinglyLinkedList = DoublyLinkedList<Int>()
+    fun `given empty list, the head the tail are null, and count is 0`() {
+        val emptyDoublyLinkedList = DoublyLinkedList<Int>()
 
         val expectedCount = 0
-        assertNull(emptySinglyLinkedList.head)
-        assertEquals(expectedCount, emptySinglyLinkedList.count)
+        assertNull(emptyDoublyLinkedList.head)
+        assertNull(emptyDoublyLinkedList.tail)
+        assertEquals(expectedCount, emptyDoublyLinkedList.count)
     }
     //endregion
+
+    //region addHead tests
+    @Test
+    fun `given empty list, when add head is called once, count is updated and head is equal to tail and not null`() {
+
+        // GIVEN
+        val valueToInsert = 5
+        val emptyDoublyLinkedList = DoublyLinkedList<Int>()
+
+        // WHEN
+        emptyDoublyLinkedList.addHead(valueToInsert)
+
+        // THEN
+        val expectedCount = 1
+        assertNotNull(emptyDoublyLinkedList.head)
+        assertNotNull(emptyDoublyLinkedList.tail)
+        assertNull(emptyDoublyLinkedList.head!!.next)
+        assertNull(emptyDoublyLinkedList.head!!.previous)
+        assertNull(emptyDoublyLinkedList.tail!!.previous)
+        assertEquals(emptyDoublyLinkedList.head, emptyDoublyLinkedList.tail)
+        assertEquals(expectedCount, emptyDoublyLinkedList.count)
+        assertEquals(valueToInsert, emptyDoublyLinkedList.head!!.value)
+    }
+
+    @Test
+    fun `given list with one item, when add head is called, count, head and tail are updated`() {
+
+        // GIVEN
+        val valueToInsert = 5
+        val doublyLinkedList = DoublyLinkedList<Int>()
+        doublyLinkedList.addHead(1)
+
+        // WHEN
+        doublyLinkedList.addHead(valueToInsert)
+
+        // THEN
+        val expectedCount = 2
+        assertNotNull(doublyLinkedList.head)
+        assertNotNull(doublyLinkedList.tail)
+        assertEquals(expectedCount, doublyLinkedList.count)
+        assertEquals(valueToInsert, doublyLinkedList.head!!.value)
+        assertEquals(1, doublyLinkedList.tail!!.value)
+    }
+    //endregion
+
 }
