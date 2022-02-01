4cd48a5b6b91d9cf7c666bd4c55d4539c6e27403
Remon Shehata <RemonGerges94@gmail.com>
2022-02-02 00:14:42 +0200

    Add addTail tests for DoublyLinkedList

--- src/test/kotlin/ds/linked_list/DoublyLinkedListNodeTest.kt
+++ src/test/kotlin/ds/linked_list/DoublyLinkedListNodeTest.kt
@@ -61,4 +61,47 @@ internal class DoublyLinkedListNodeTest {
     }
     //endregion
 
+    //region addTail tests
+    @Test
+    fun `given empty list, when addTail is called once, count is updated and head is equal to tail and not null`() {
+
+        // GIVEN
+        val valueToInsert = 5
+        val emptyDoublyLinkedList = DoublyLinkedList<Int>()
+
+        // WHEN
+        emptyDoublyLinkedList.addTail(valueToInsert)
+
+        // THEN
+        val expectedCount = 1
+        assertNotNull(emptyDoublyLinkedList.head)
+        assertNotNull(emptyDoublyLinkedList.tail)
+        assertNull(emptyDoublyLinkedList.head!!.next)
+        assertNull(emptyDoublyLinkedList.head!!.previous)
+        assertNull(emptyDoublyLinkedList.tail!!.previous)
+        assertEquals(emptyDoublyLinkedList.head, emptyDoublyLinkedList.tail)
+        assertEquals(expectedCount, emptyDoublyLinkedList.count)
+        assertEquals(valueToInsert, emptyDoublyLinkedList.head!!.value)
+    }
+
+    @Test
+    fun `given list with one item, when addTail is called, count, head and tail are updated`() {
+
+        // GIVEN
+        val valueToInsert = 5
+        val doublyLinkedList = DoublyLinkedList<Int>()
+        doublyLinkedList.addHead(1)
+
+        // WHEN
+        doublyLinkedList.addTail(valueToInsert)
+
+        // THEN
+        val expectedCount = 2
+        assertNotNull(doublyLinkedList.head)
+        assertNotNull(doublyLinkedList.tail)
+        assertEquals(expectedCount, doublyLinkedList.count)
+        assertEquals(valueToInsert, doublyLinkedList.tail!!.value)
+        assertEquals(1, doublyLinkedList.head!!.value)
+    }
+    //endregion
 }
