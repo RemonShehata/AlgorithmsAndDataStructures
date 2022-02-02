cd9389d10a6b36091cb0284ed106b65d5e08b8b1
Remon Shehata <RemonGerges94@gmail.com>
2022-02-02 22:10:59 +0200

    add functions to the interface add documentation to the interface

--- src/main/kotlin/ds/linked_list/ISortedList.kt
+++ src/main/kotlin/ds/linked_list/ISortedList.kt
@@ -1,9 +1,80 @@
 package ds.linked_list
 
-interface ISortedList<T : Comparable<T>> {
+/**
+ * Doubly linked list where items on the left are lesser than items on the right.
+ */
+interface ISortedList<T : Comparable<T>>: Iterator<T> {
+
+    /**
+     * adds [value] in sort order in [ISortedList].
+     */
     fun add(value: T)
+
+    /**
+     * adds [SortedListNode] to the beginning of the [ISortedList].
+     */
     fun add(node: SortedListNode<T>)
 
-//    fun remove(value: T)
-//    fun remove(node: SortedListNode<T>)
+    /**
+     * removes the first node that contains [value].
+     * @param [value] the value to be removed.
+     * @return true if the value was removed.
+     * @return false if the value did not exist.
+     */
+    fun remove(value: T): Boolean
+
+    /**
+     * removes the first node that contains value equal to [node].
+     * @param [node] the node to be removed.
+     * @return true if the value was removed.
+     * @return false if the value did not exist.
+     */
+    fun remove(node: SortedListNode<T>): Boolean
+
+    /**
+     * check the [ISortedList] contains a specific value.
+     * @return true if the value exist.
+     * @return false if the value doesn't exist.
+     */
+    fun contains(value: T): Boolean
+
+    /**
+     * check the [ISortedList] contains a specific node with value equal to [node]'s value.
+     * @return true if the value exist.
+     * @return false if the value doesn't exist.
+     */
+    fun contains(node: SortedListNode<T>): Boolean
+
+    /**
+     * finds [value] in the [ISortedList].
+     * @return the first [SortedListNode] if it existed.
+     * @return null if it doesn't exist.
+     */
+    fun find(value: T): SortedListNode<T>?
+
+    /**
+     * finds the value of [node] in the [ISortedList].
+     * @return the first [SortedListNode] if it existed.
+     * @return null if it doesn't exist.
+     */
+    fun find(node: SortedListNode<T>): SortedListNode<T>?
+
+    /**
+     * Returns the index of the first occurrence of the specified element in the list,
+     * or -1 if the specified element is not contained in the [ISortedList].
+     * note that we are a zero index based list.
+     */
+    fun indexOf(value: T): Int
+
+    /**
+     * Returns the index of the first occurrence of the specified element in the list,
+     * or -1 if the specified element is not contained in the [ISortedList].
+     * note that we are a zero index based list.
+     */
+    fun indexOf(node: SortedListNode<T>): Int
+
+    /**
+     * reset everything.
+     */
+    fun clear()
 }
