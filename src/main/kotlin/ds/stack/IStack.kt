1757bab4b86777effba1b1fdf3735c6de4a532a4
Remon Shehata <RemonGerges94@gmail.com>
2022-02-06 20:27:40 +0200

    add Stack interface

---
+++ src/main/kotlin/ds/stack/IStack.kt
@@ -0,0 +1,54 @@
+package ds.stack
+
+interface IStack<E> {
+
+    /**
+     * Pushes an item onto the top of this stack.
+     *
+     * @param   item   the item to be pushed onto this stack.
+     * @return  the <code>item</code> argument.
+     */
+    fun push(item: E)
+
+    /**
+     * Removes the object at the top of this stack and returns that
+     * object as the value of this function.
+     *
+     * @return  The object at the top of this stack.
+     * @throws  IllegalStateException  if this stack is empty.
+     */
+    fun pop(): E
+
+    /**
+     * Looks at the object at the top of this stack without removing it
+     * from the stack.
+     *
+     * @return  the object at the top of this stack.
+     * @throws  IllegalStateException  if this stack is empty.
+     */
+    fun peek(): E
+
+    /**
+     * Tests if this stack is empty.
+     *
+     * @return  <code>true</code> if and only if this stack contains
+     *          no items; <code>false</code> otherwise.
+     */
+    fun isEmpty(): Boolean
+
+    /**
+     * Returns the 1-based position where an object is on this stack.
+     * If the object <tt>o</tt> occurs as an item in this stack, this
+     * method returns the distance from the top of the stack of the
+     * occurrence nearest the top of the stack; the topmost item on the
+     * stack is considered to be at distance <tt>1</tt>. The <tt>equals</tt>
+     * method is used to compare <tt>o</tt> to the
+     * items in this stack.
+     *
+     * @param   o   the desired object.
+     * @return  the 1-based position from the top of the stack where
+     *          the object is located; the return value <code>-1</code>
+     *          indicates that the object is not on the stack.
+     */
+    fun indexOf(item: E): Int
+}
