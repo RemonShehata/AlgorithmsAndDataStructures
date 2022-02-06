fbba57f5d22347a81cc35281ed1f751c9f4a6c35
Remon Shehata <RemonGerges94@gmail.com>
2022-02-06 23:03:25 +0200

    add all Queue tests

---
+++ src/test/kotlin/ds/queue/QueueTest.kt
@@ -0,0 +1,152 @@
+package ds.queue
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class QueueTest{
+    //region count
+    @Test
+    fun `given empty queue, when count is called, it is zero`() {
+        val queue = Queue<Int>()
+
+        assertEquals(0, queue.count)
+    }
+    //endregion
+
+    //region enqueue
+    @Test
+    fun `given empty queue, when enqueue is called once, count is updated`() {
+
+        // GIVEN
+        val valueToInsert = 5
+        val queue = Queue<Int>()
+
+        // WHEN
+        queue.enqueue(valueToInsert)
+
+        // THEN
+        val expectedCount = 1
+        assertEquals(expectedCount, queue.count)
+    }
+
+    @Test
+    fun `given queue with one item, when enqueue is called once, count is updated`() {
+
+        // GIVEN
+        val valueToInsert = 5
+        val queue = Queue<Int>()
+        queue.enqueue(1)
+
+        // WHEN
+        queue.enqueue(valueToInsert)
+
+        // THEN
+        val expectedCount = 2
+        assertEquals(expectedCount, queue.count)
+    }
+    //endregion
+
+    //region dequeue
+    @Test
+    fun `given empty queue, when dequeue is called, exception is thrown`() {
+
+        // GIVEN
+        val queue = Queue<Int>()
+
+        // THEN
+        assertThrows<IllegalStateException> { queue.dequeue() }
+    }
+
+    @Test
+    fun `given queue with one item, when dequeue is called, count is updated`() {
+
+        // GIVEN
+        val queue = Queue<Int>()
+        queue.enqueue(1)
+
+        // WHEN
+        queue.dequeue()
+
+        // THEN
+        val expectedCount = 0
+        assertEquals(expectedCount, queue.count)
+    }
+    //endregion
+
+    //region peek
+    @Test
+    fun `given empty queue, when peek is called, exception is thrown`() {
+
+        // GIVEN
+        val queue = Queue<Int>()
+
+        // THEN
+        assertThrows<IllegalStateException> { queue.peek() }
+    }
+
+    @Test
+    fun `given queue with one item, when peek is called, the head value is returned and count is the same`() {
+
+        // GIVEN
+        val queue = Queue<Int>()
+        queue.enqueue(10)
+
+        // WHEN
+        val result = queue.peek()
+
+        // THEN
+        val expectedCount = 1
+        val expectedHeadValue = 10
+        assertEquals(expectedCount, queue.count)
+        assertEquals(expectedHeadValue, result)
+    }
+
+    @Test
+    fun `given a multi item-queue, when peek is called, the head value is returned and count is the same`() {
+
+        // GIVEN
+        val queue = Queue<Int>()
+        queue.enqueue(10)
+        queue.enqueue(20)
+
+        // WHEN
+        val result = queue.peek()
+
+        // THEN
+        val expectedCount = 2
+        val expectedHeadValue = 10
+        assertEquals(expectedCount, queue.count)
+        assertEquals(expectedHeadValue, result)
+    }
+    //endregion
+
+    //region isEmpty
+    @Test
+    fun `given empty queue, when isEmpty is called, it returns true`() {
+
+        // GIVEN
+        val queue = Queue<Int>()
+
+        // WHEN
+        val result = queue.isEmpty()
+
+        // THEN
+        assertTrue(result)
+    }
+
+    @Test
+    fun `given non-empty queue, when isEmpty is called, it returns false`() {
+
+        // GIVEN
+        val queue = Queue<Int>()
+        queue.enqueue(1)
+
+        // WHEN
+        val result = queue.isEmpty()
+
+        // THEN
+        assertFalse(result)
+    }
+    //endregion
+}
