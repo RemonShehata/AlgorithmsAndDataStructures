77065f4dc13ac1018a2d2d7506e8be368e787132
Remon Shehata <RemonGerges94@gmail.com>
2022-02-08 14:07:11 +0200

    add dokka for the tree and the node

--- src/main/kotlin/ds/tree/BinarySearchTree.kt
+++ src/main/kotlin/ds/tree/BinarySearchTree.kt
@@ -1,10 +1,17 @@
 package ds.tree
 
+/**
+ * a node that contains a single data item and pointers to the left and right child.
+ */
 class BinarySearchTreeNode<T : Comparable<T>>(
     val data: T,
     var left: BinarySearchTreeNode<T>? = null,
     var right: BinarySearchTreeNode<T>? = null
 )
 
+/**
+ * binary tree where nodes with less value are stored on the left.
+ * And, greater than or equal to values are stores on the right.
+ */
 class BinarySearchTree<T> : IBinarySearchTree<T> {
 }
