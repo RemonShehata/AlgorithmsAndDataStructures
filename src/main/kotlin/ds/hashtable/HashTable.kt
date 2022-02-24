fbba8aedd72dad12f04b7aa8b71369d2898f2864
Remon Shehata <RemonGerges94@gmail.com>
2022-02-24 16:15:55 +0200

    fix a bug with HashTable clear function we don't need to clear SinglyLinkedList

--- src/main/kotlin/ds/hashtable/HashTable.kt
+++ src/main/kotlin/ds/hashtable/HashTable.kt
@@ -168,10 +168,8 @@ class HashTable<K, V>(initialCapacity: Int?) : IHashTable<K, V> {
 
     override fun clear() {
         for (i in entries.indices) {
-            entries[i]?.clear()
             entries[i] = null
         }
-
         count = 0
     }
 
