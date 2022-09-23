6c56a0eed948f1a15caf66c0b0d0a45c2ca003f1
Remon Shehata <RemonGerges94@gmail.com>
2022-09-23 15:40:57 +0200

    implement insertion sort

--- src/main/kotlin/algo/Sorting.kt
+++ src/main/kotlin/algo/Sorting.kt
@@ -35,4 +35,15 @@ fun bubbleSort2(array: Array<Int>) {
         }
     }
 }
+
+fun insertionSort(array: Array<Int>) {
+    for (i in 1 until array.size) {
+        val currentValue = array[i]
+        var j = i - 1
+        while (j >= 0 && array[j] > currentValue) {
+            array[j + 1] = array[j]
+            j--
+        }
+        array[j + 1] = currentValue
+    }
 }
