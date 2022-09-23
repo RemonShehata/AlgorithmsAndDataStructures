223f975e0ebbf19db1270a2ca46aa901a78b6c65
Remon Shehata <RemonGerges94@gmail.com>
2022-09-23 11:21:40 +0200

    add testing for bubbleSort 2

--- src/test/kotlin/algo/SortingKtTest.kt
+++ src/test/kotlin/algo/SortingKtTest.kt
@@ -45,5 +45,46 @@ internal class SortingKtTest {
         assert(inputArray.contentEquals(expected))
     }
     //endregion
+
+    //region bubbleSort2
+    @Test
+    fun ` given non sorted array when bubbleSort2 is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(5, 4, 3, 2, 1)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        bubbleSort2(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+
+    @Test
+    fun ` given sorted array when bubbleSort2 is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(1, 2, 3, 4, 5)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        bubbleSort2(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+
+    @Test
+    fun ` given half sorted array when bubbleSort2 is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(1, 2, 5, 4, 3)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        bubbleSort2(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+    //endregion
 }
 
