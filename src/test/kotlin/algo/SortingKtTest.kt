a1fa26deb70282868a8ce5e4122b62d5b227aabd
Remon Shehata <RemonGerges94@gmail.com>
2022-09-23 15:41:08 +0200

    add unit tests for insertionSort

--- src/test/kotlin/algo/SortingKtTest.kt
+++ src/test/kotlin/algo/SortingKtTest.kt
@@ -86,5 +86,46 @@ internal class SortingKtTest {
         assert(inputArray.contentEquals(expected))
     }
     //endregion
+
+    //region insertionSort
+    @Test
+    fun ` given non sorted array when insertionSort is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(5, 4, 3, 2, 1)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        insertionSort(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+
+    @Test
+    fun ` given sorted array when insertionSort is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(1, 2, 3, 4, 5)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        insertionSort(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+
+    @Test
+    fun ` given half sorted array when insertionSort is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(1, 2, 5, 4, 3)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        insertionSort(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+    //endregion
 }
 
