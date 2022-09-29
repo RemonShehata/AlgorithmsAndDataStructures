ddaa593ee199db320552303f6e1fcfeefee3b56a
Remon Shehata <RemonGerges94@gmail.com>
2022-09-29 10:59:07 +0200

    add unit tests for mergeSort

--- src/test/kotlin/algo/SortingKtTest.kt
+++ src/test/kotlin/algo/SortingKtTest.kt
@@ -127,5 +127,46 @@ internal class SortingKtTest {
         assert(inputArray.contentEquals(expected))
     }
     //endregion
+
+    //region mergeSort
+    @Test
+    fun ` given non sorted array when mergeSort is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(5, 4, 3, 2, 1)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        mergeSort(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+
+    @Test
+    fun ` given sorted array when mergeSort is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(1, 2, 3, 4, 5)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        mergeSort(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+
+    @Test
+    fun ` given half sorted array when mergeSort is called the array is sorted`() {
+        // GIVEN
+        val inputArray = arrayOf(1, 2, 5, 4, 3)
+        val expected = arrayOf(1, 2, 3, 4, 5)
+
+        // WHEN
+        mergeSort(inputArray)
+
+        //THEN
+        assert(inputArray.contentEquals(expected))
+    }
+    //endregion
 }
 
